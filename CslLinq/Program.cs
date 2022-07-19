586d302af40dfbe7e6ebc5b83d5a8c6a94649851
Moustafa moungari <moungari.moustafa@gmail.com>
2022-07-19 23:43:44 +0200

    fonction synchrone et fonction asynchrone delegate

--- CslLinq/Program.cs
+++ CslLinq/Program.cs
@@ -1,131 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace CslLinq
 {
     internal class Program
     {
+        private static bool isOk =false;
 
         public delegate void delSuperieur(Personne p);
         public delegate void delquestionUser(string message);
-    
+
         static void Main(string[] args)
         {
 
+            // fonction synchrone
+
+            var webClient = new WebClient();
+            Console.WriteLine("Telechargement.....");
+            string url = "https://sm.mashable.com/mashable_in/seo/2/24308/24308_tz9f.jpg";
+            //webClient.DownloadFile(url, "pierre.jpg");
+        
+
+            webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+            webClient.DownloadFileAsync(new Uri(url), "24308_tz9f.jpg");
+
+            while (true)
+            {
+                if (isOk) { break; }
+            }
+
+            Console.WriteLine("Fin du programme");
+        }
+
+        private static void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
             
-            IList<Personne> personnlist = new List<Personne>();
-
-            personnlist.Add(new Personne("Robert", 13, "Oxford", "England"));
-            personnlist.Add(new Personne("Selma", 19, "Dole", "France"));
-            personnlist.Add(new Personne("Albert", 34, "Oxford", "England"));
-            personnlist.Add(new Personne("Wassila", 25, "Paris", "France"));
-            personnlist.Add(new Personne("Brahim", 36, "Dammarie les lys", "France"));
-            personnlist.Add(new Personne("Ismail", 9, "Dubai", "Dubai"));
-            personnlist.Add(new Personne("Francoise", 78, "Bouzole", "France"));
-            personnlist.Add(new Personne("Mohamed", 45, "Damparis", "France"));
-
-            Personne p = new Personne("Robert", 23, "Oxford", "England");
-
-            //Delegate Action....
-            Action<Personne> delSuperieurAction = p.isBiggerOfPersonne;
-
-           // declaration du delegate
-            delSuperieur delSuperieur = new delSuperieur(p.isBiggerOfPersonne);
-            // on recupere la liste des personnes dont l'age est superieur a 0
-            var listPers =  personnlist.Where(p => p.age > 0).ToList();
-
-            Console.WriteLine("-------------------------- Action ---------------------------------");
-            foreach (Personne p1 in listPers)
-            {
-                //delSuperieur(p1);
-                delSuperieurAction(p1);
-            }
-
-            Console.WriteLine("--------------------------delegate appel method---------------------------------");
-            delquestionUser delquestionUser = questionUtilisateur;
-            //delquestionUser("Veuillez indiquer votre nom ?");
-            //delquestionUser = questionUtilisateurAge;
-            //delquestionUser("Veuillez saisir votre age?");
-
-
-            Console.WriteLine("--------------------------PREDICATE---------------------------------");
-            Predicate<int> verifAge = s => s > 23;
-
-                Random rnd = new Random();
-                var valeur =  rnd.Next(10, 45);
-
-            if (verifAge(valeur))
-            {
-                Console.WriteLine($"Valeur valid : {valeur}");
-            }
-            else
-            {
-                Console.WriteLine($"Valeur invalid : {valeur}");
-            }
-            Console.WriteLine("--------------------------Func---------------------------------");
-
-            Func<int, int, int> calc = addition;
-
-            Console.WriteLine($"Resultat de l'addition : {calc(valeur, valeur)}");
+            Console.WriteLine("Telechargement terminé");
+            isOk = true;
 
         }
-
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="message"></param>
-        static public void questionUtilisateur(string message)
-        {
-
-            Console.WriteLine(message);
-            var result = Console.ReadLine();
-
-            if (result != null)
-            {
-                Console.WriteLine($"votre nom est : {result}");
-            }
-
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="message"></param>
-        static public void questionUtilisateurAge(string message)
-        {
-            int age = 0;
-            Console.WriteLine(message);
-            var result = Console.ReadLine();
-
-            bool isValidAge =  int.TryParse(result, out age);
-            if (result != null )
-            {
-                if(age > 18)
-                {
-                    Console.WriteLine("Vous etes majeur ");
-                    Console.WriteLine($"votre age est  : {age} ans");
-                    Console.WriteLine("Autorisation valider ");
-                }
-                else
-                {
-                    Console.WriteLine("Vous etes mineur ,autorisation refuser ");
-                }
-               
-            }
-
-        }
-
-
-        static public int addition(int val,int val2)
-        {
-            return val + val2;
-        }
-
-
-
-
-
     }
 }
