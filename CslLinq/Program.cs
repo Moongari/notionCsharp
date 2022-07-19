51b155908b0268918dcb70de437aa6d3642671e4
Moustafa moungari <moungari.moustafa@gmail.com>
2022-07-19 22:54:41 +0200

    delegate func / action /predicate

--- CslLinq/Program.cs
+++ CslLinq/Program.cs
@@ -35,22 +35,48 @@ namespace CslLinq
             // on recupere la liste des personnes dont l'age est superieur a 0
             var listPers =  personnlist.Where(p => p.age > 0).ToList();
 
+            Console.WriteLine("-------------------------- Action ---------------------------------");
             foreach (Personne p1 in listPers)
             {
                 //delSuperieur(p1);
                 delSuperieurAction(p1);
             }
 
-
+            Console.WriteLine("--------------------------delegate appel method---------------------------------");
             delquestionUser delquestionUser = questionUtilisateur;
-            delquestionUser("Veuillez indiquer votre nom ?");
+            //delquestionUser("Veuillez indiquer votre nom ?");
+            //delquestionUser = questionUtilisateurAge;
+            //delquestionUser("Veuillez saisir votre age?");
 
+
+            Console.WriteLine("--------------------------PREDICATE---------------------------------");
+            Predicate<int> verifAge = s => s > 23;
+
+                Random rnd = new Random();
+                var valeur =  rnd.Next(10, 45);
+
+            if (verifAge(valeur))
+            {
+                Console.WriteLine($"Valeur valid : {valeur}");
+            }
+            else
+            {
+                Console.WriteLine($"Valeur invalid : {valeur}");
+            }
+            Console.WriteLine("--------------------------Func---------------------------------");
+
+            Func<int, int, int> calc = addition;
+
+            Console.WriteLine($"Resultat de l'addition : {calc(valeur, valeur)}");
 
         }
 
 
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
         static public void questionUtilisateur(string message)
         {
 
@@ -63,6 +89,43 @@ namespace CslLinq
             }
 
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        static public void questionUtilisateurAge(string message)
+        {
+            int age = 0;
+            Console.WriteLine(message);
+            var result = Console.ReadLine();
+
+            bool isValidAge =  int.TryParse(result, out age);
+            if (result != null )
+            {
+                if(age > 18)
+                {
+                    Console.WriteLine("Vous etes majeur ");
+                    Console.WriteLine($"votre age est  : {age} ans");
+                    Console.WriteLine("Autorisation valider ");
+                }
+                else
+                {
+                    Console.WriteLine("Vous etes mineur ,autorisation refuser ");
+                }
+               
+            }
+
+        }
+
+
+        static public int addition(int val,int val2)
+        {
+            return val + val2;
+        }
+
+
+
+
 
     }
 }
