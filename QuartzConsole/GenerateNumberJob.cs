32cbd00681a1713228b4e49477bd05b5950f7685
Moustafa moungari <moungari.moustafa@gmail.com>
2023-01-09 10:20:24 +0100

    Creation d'un projet utilisant Quartz.net

---
+++ QuartzConsole/GenerateNumberJob.cs
@@ -0,0 +1,28 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzConsole
+{
+    public class GenerateNumberJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Random value : {RandomNumber(10,200)}");
+            return Task.CompletedTask;
+        }
+
+
+        private int RandomNumber(int min, int max)
+        {
+            var rand = new Random();
+            return rand.Next(min, max);
+        }
+
+
+    }
+
+}
---
+++ QuartzConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//1 Create a Scheduler Factory
+    using Quartz;
+    using Quartz.Impl;
+using QuartzConsole;
+using System.Security.Cryptography.X509Certificates;
+
+// 1 Create a scheduler Factory
+ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+// 2 Get and Start Scheduler
+IScheduler scheduler = await schedulerFactory.GetScheduler();
+await scheduler.Start();
+//3 Create a Job
+IJobDetail job = JobBuilder.Create<GenerateNumberJob>().WithIdentity("Number generator Job", "Number gen Group").Build();
+//4 Create a Trigger
+ITrigger trigger = TriggerBuilder.Create()
+    .WithIdentity("Number generator trigger", "Number generator group")
+    .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever()).Build();
+// Schedule the job using the job and trigger
+
+await scheduler.ScheduleJob(job,trigger);
+
+Console.ReadKey();
+
